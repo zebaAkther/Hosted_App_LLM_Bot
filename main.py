ee1fa8f8d126d9231ff0f13fd7b7129831a366a2
zebaAkther <zebaakther94@gmail.com>
2025-09-06 10:37:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ FRONTEND_URL = "relaxed-macaron-0ceca5.netlify.app"
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL],  # Allow only your frontend domain for security
+    allow_origins=["*"],  # Allow only your frontend domain for security
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
